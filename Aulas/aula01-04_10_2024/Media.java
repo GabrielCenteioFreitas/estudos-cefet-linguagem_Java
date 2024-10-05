8d65a42cff83fd2b6675e4a00c9a954c33f53c63
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-10-05 05:07:04 +0000

    adicionando os exercícios da Aula 01

---
+++ Aulas/aula01-04_10_2024/Media.java
@@ -0,0 +1,7 @@
+public class Media {
+  public static void main(String[] args) {
+    int num1 = Integer.parseInt(args[0]), num2 = Integer.parseInt(args[1]);
+
+    System.out.println((num1 + num2) / 2);
+  }
+}
